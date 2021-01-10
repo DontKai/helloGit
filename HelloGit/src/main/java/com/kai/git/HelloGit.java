f09747dfe954328bb24609a5e099a2a68708c57f
DontKai <1403698978@qq.com>
2021-01-10 18:45:09 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/kai/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.kai.git;
+
+/**
+ * @author DK
+ * @date 2021-01-10 17:46
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
