fd1bfd0a430591022e11a7415aa824e3821ce6d0
DontKai <1403698978@qq.com>
2021-01-10 19:16:16 +0800

    创建分支

--- HelloGit/src/main/java/com/kai/git/HelloGit.java
+++ HelloGit/src/main/java/com/kai/git/HelloGit.java
@@ -7,7 +7,7 @@ package com.kai.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-
         System.out.println("更新1");
+        System.out.println("创建分支");
     }
 }
